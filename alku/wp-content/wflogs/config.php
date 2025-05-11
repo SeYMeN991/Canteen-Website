108606798bad710c0db2ce534d47263e2e3248d3
SeYMeN991 <seymenh.99@gmail.com>
2025-05-11 21:50:45 +0300

    Canteen Website

---
+++ alku/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"8&)25,8Kh8gQq8q.#a%dL:kCP1O<U{05:4V7Q1 Snzq@.5!*$.tOV`$NhuZ%G`&x";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1622;}
